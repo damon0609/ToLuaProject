fb8cd8f60a2f0a670a542068912862051c1723ef
damon <903005766@qq.com>
2018-12-23 17:26:19 +0800

    第一次提交

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public LuaEngine luaEngine;
+
+	void Start () {
+
+        luaEngine = GetComponent<LuaEngine>();
+
+
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/LuaEngine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LuaInterface;
+using UnityEngine;
+
+public class LuaEngine : LuaClient
+{
+
+    protected override LuaFileUtils InitLoader()
+    {
+        return new LuaResLoader();
+    }
+
+    protected override void LoadLuaFiles()
+    {
+        base.LoadLuaFiles();
+        luaState.AddSearchPath(Application.dataPath+"/lua");
+        OpenLibs();
+    }
+}
